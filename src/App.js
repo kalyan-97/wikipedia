f4962d57382fb2d7493b8d4c3a2856c8e212c174
kalyan_97 <you@example.com>
2024-03-14 12:31:03 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  const [searchItem,setSearchItem] = useState([])
+  const [search,setSearch] = useState("google")
+  
+  const getFetchedItems = async() => {
+        const response = await fetch(`https://apis.ccbp.in/wiki-search?search=${search}`);
+        const data = await response.json();
+        //console.log(data.search_results)
+        setSearchItem(data.search_results)
+  }
+
+  useEffect(() =>{
+      getFetchedItems()
+  },[search])
+
+  const handleSearch = (e) =>{
+      e.preventDefault();
+      setSearch(e.target.value);
+  }
+
+  console.log(searchItem)
+
+  return(
+    <section>
+      <div className="top-container">
+        <img 
+        className="logo"
+        alt="logo"
+        src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/wiki-logo-img.png" />
+        <br/>
+        <div className="input-container"> 
+        <input onChange={handleSearch}  placeholder="search" type="text"/>
+        <i class="fa fa-search" aria-hidden="true"></i>
+        </div>
+      </div>
+      <hr/>
+      {
+        searchItem.map((x) =>{
+          const {title,link,description} = x;
+          return(
+            <div className="result-container">
+              <a className="title" href="link" target="_blank"> {title} </a> <br/>
+              <a className="link" href="link" target="_blank"> {link} </a>
+              <p className="description"> {description} </p>
+            </div>
+          )
+        })
+      }
+    </section>
+  )
 }
 
 export default App;
